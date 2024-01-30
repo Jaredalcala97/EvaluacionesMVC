7856817c4cdb100af3ce5e19b93c3e8c37d1bc1c
Jaredalcala97 <JaredHelaman.Alcala@aero.bombardier.com>
2024-01-30 15:23:20 -0600

    CTP-0009
    borrado

--- app/app/Controllers/HomeController.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class HomeController
-    Inherits System.Web.Mvc.Controller
-
-    Function Index() As ActionResult
-        Return View()
-    End Function
-
-
-End Class
--- app/app/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.vb" Inherits="app.MvcApplication" Language="VB" %>
